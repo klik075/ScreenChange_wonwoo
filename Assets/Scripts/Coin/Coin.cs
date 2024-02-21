1827e44b656ca12b5e149a1ad701e7b16081658c
정원우 <54103715+klik075@users.noreply.github.com>
2024-02-21 12:11:39 +0900

    Feat - 타일맵 적용 , 투명 벽 콜라이더 추가

--- Assets/Scripts/Coin/Coin.cs
+++ Assets/Scripts/Coin/Coin.cs
@@ -4,13 +4,8 @@ using UnityEngine;
 
 public class Coin : MonoBehaviour
 {
-    private AudioSource audioSource;
     private int count = 1; //증가할 코인의 수
-    // Start is called before the first frame update
-    void Start()
-    {
-        audioSource = GetComponent<AudioSource>();
-    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.GetComponent<Player>() != null)
